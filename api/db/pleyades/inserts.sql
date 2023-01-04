288daa9d2638f18635d1f5e0101967cde5c78331
Juan Quintero <juanestquintero@gamil.com>
2023-01-04 08:03:35 -0500

    chore(sql): test users inserts

--- api/db/pleyades/inserts.sql
+++ api/db/pleyades/inserts.sql
@@ -2,5 +2,4 @@ use pleyades;
     
 INSERT INTO `pleyades`.`usuarios`(`nombre`,`correo`,`clave`,`rol`) VALUES
 ('SUPER ADMIN','admin@pleyades.com','25d55ad283aa400af464c76d713c07ad','Admin'),
-('JUAN ALBERTO PEREZ','albert.jhon@gmail.com','25d55ad283aa400af464c76d713c07ad','Analista'),
-('CLAUDIA JOSEFINA LOPEZ','josephine.claud@gmail.com','25d55ad283aa400af464c76d713c07ad','Analista');
+('Walter Alvarez ','walter.alvarez@educatic.com.co','25d55ad283aa400af464c76d713c07ad','Analista');
