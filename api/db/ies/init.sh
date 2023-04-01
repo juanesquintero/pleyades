c2232aaeed4b617b305c7c360001b3b87df58af1
Juan Quintero <juanestquintero@gamil.com>
2023-04-01 18:58:23 -0500

    feat: init script for database inserts

--- api/db/ies/init.sh
+++ api/db/ies/init.sh
@@ -1,7 +1,29 @@
-/bin/bash -c "
-    /opt/mssql/bin/sqlservr &
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -i /tmp/init.sql &
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U $MSSQL_DBUSER -P $MSSQL_DBUSERPWD -i /tmp/creates.sql &
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U $MSSQL_DBUSER -P $MSSQL_DBUSERPWD -i /tmp/inserts/dependencias.sql &
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U $MSSQL_DBUSER -P $MSSQL_DBUSERPWD -i /tmp/inserts/desercion.sql
-"
+## Open permissions to this file
+chmod +x /tmp/init.sh
+
+YELLOW=$(tput setaf 3)
+NC=$(tput setaf 7)
+
+echo "${YELLOW}Inicializando la base de datos de deserción de SQL Server...${NC}"
+
+## Run MSSQLServer service
+/opt/mssql/bin/sqlservr
+
+## Reuse variables
+sqlcmd='/opt/mssql-tools/bin/sqlcmd -S localhost -U'
+
+sqlcmd_sa=$sqlcmd" sa -P $MSSQL_SA_PASSWORD -i"
+sqlcmd_user=$sqlcmd" $MSSQL_DBUSER -P $MSSQL_DBUSERPWD -i"
+
+
+## Execute initial sql files
+
+$sqlcmd_sa /tmp/sql/init.sql
+
+$sqlcmd_sa /tmp/sql/creates.sql
+
+$sqlcmd_user /tmp/sql/inserts/dependencias.sql
+
+$sqlcmd_user /tmp/sql/inserts/desercion.sql
+
+echo "${YELLOW}Finalizó la definicion de la bd de deserción!!!${NC}"
