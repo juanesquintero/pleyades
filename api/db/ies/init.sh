ffd7897f760503c178ed79ed752737c63006196e
Juan Quintero <juanestquintero@gamil.com>
2023-04-01 19:23:03 -0500

    chore: init.sh messages on step

--- api/db/ies/init.sh
+++ api/db/ies/init.sh
@@ -1,11 +1,6 @@
 ## Open permissions to this file
 chmod +x /tmp/init.sh
 
-YELLOW=$(tput setaf 3)
-NC=$(tput setaf 7)
-
-echo "${YELLOW}Inicializando la base de datos de deserción de SQL Server...${NC}"
-
 ## Run MSSQLServer service
 /opt/mssql/bin/sqlservr
 
@@ -15,15 +10,21 @@ sqlcmd='/opt/mssql-tools/bin/sqlcmd -S localhost -U'
 sqlcmd_sa=$sqlcmd" sa -P $MSSQL_SA_PASSWORD -i"
 sqlcmd_user=$sqlcmd" $MSSQL_DBUSER -P $MSSQL_DBUSERPWD -i"
 
+YELLOW=$(tput setaf 3)
+NC=$(tput setaf 7)
 
 ## Execute initial sql files
 
+echo "${YELLOW}Inicializando la base de datos de deserción de SQL Server...${NC}"
 $sqlcmd_sa /tmp/sql/init.sql
 
+echo "${YELLOW}Ejecutando creates.sql...${NC}"
 $sqlcmd_sa /tmp/sql/creates.sql
 
+echo "${YELLOW}Ejecutando inserts dependencias.sql...${NC}"
 $sqlcmd_user /tmp/sql/inserts/dependencias.sql
 
+echo "${YELLOW}Ejecutando inserts desercion.sql...${NC}"
 $sqlcmd_user /tmp/sql/inserts/desercion.sql
 
 echo "${YELLOW}Finalizó la definicion de la bd de deserción!!!${NC}"
