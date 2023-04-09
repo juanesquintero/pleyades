4ae3158ce27f2e2d8b8e3d65bfd6c57b0a5b0868
Juan Quintero <juanestquintero@gamil.com>
2023-04-09 10:09:14 -0500

    refactor: api result endpoint logger

--- web/services/API.py
+++ web/services/API.py
@@ -13,7 +13,7 @@ error_logger = logging.getLogger('error_logger')
 def get(endpoint):
     res = requests.get(api_path+endpoint, headers=session.get('headers'))
     status, body = res.status_code, res.json()
-    return result(status, body)
+    return result(endpoint, status, body)
 
 
 def post(endpoint, body_json):
@@ -23,7 +23,7 @@ def post(endpoint, body_json):
         headers=session.get('headers')
     )
     status, body = res.status_code, res.json()
-    return result(status, body)
+    return result(endpoint, status, body)
 
 
 def put(endpoint, body_json):
@@ -33,20 +33,20 @@ def put(endpoint, body_json):
         headers=session.get('headers')
     )
     status, body = res.status_code, res.json()
-    return result(status, body)
+    return result(endpoint, status, body)
 
 
 def delete(endpoint):
     res = requests.delete(api_path+endpoint, headers=session.get('headers'))
     status, body = res.status_code, res.json()
-    return result(status, body)
+    return result(endpoint, status, body)
 
-def result(status, body):
+def result(endpoint, status, body):
     if status == 200:
         return True, body
 
     if body.get('msg') == 'Su sesión ha expirado, vuelva a loguearse':
         raise Exception(body.get('msg'))
 
-    error_logger.error(f'\nAPI ERROR... {status}: {body}\n')
+    error_logger.error(f'\nAPI ERROR...{endpoint} - {status}: {body}\n')
     return False, body
