a4e71290a4c15882a92678f6bbc6f99667bb25d6
Juan Quintero <juanestquintero@gamil.com>
2023-04-15 17:20:44 -0500

    refactor(api): desercion controllers linting

--- api/controllers/desercion/Estudiantes.py
+++ api/controllers/desercion/Estudiantes.py
@@ -13,20 +13,24 @@ db = DB.getInstance()
 
 tabla = 'VWDATADESERCION'
 
-msg_error = { 'msg': 'No hay concidencias' }, 404
+msg_error = {'msg': 'No hay concidencias'}, 404
 
 ##########################################################  VWDATADESERCION ##########################################################
+
 
 @Estudiante.route('/conjunto/<int:programa>/<int:periodoInicio>/<int:periodoFin>')
 @jwt_required()
 def get_conjunto_estudiantes(programa, periodoInicio, periodoFin):
-    sql = "SELECT * FROM {} WHERE idprograma={} AND REGISTRO >= {} AND REGISTRO <= {} ORDER BY REGISTRO;".format(tabla,programa,periodoInicio, periodoFin)
+    sql = "SELECT * FROM {} WHERE idprograma={} AND REGISTRO >= {} AND REGISTRO <= {} ORDER BY REGISTRO;".format(
+        tabla, programa, periodoInicio, periodoFin)
     query = db.select(sql)
     ex = exception(query)
-    if ex: 
+    if ex:
         return ex
-    if not(query):  return msg_error
-    return jsonify(_format(query) )
+    if not(query):
+        return msg_error
+    return jsonify(_format(query))
+
 
 @Estudiante.route('/periodo/<int:periodo>')
 @jwt_required()
@@ -34,10 +38,12 @@ def get_periodo(periodo):
     sql = 'SELECT * FROM {} WHERE REGISTRO={};'.format(tabla, periodo)
     query = db.select(sql)
     ex = exception(query)
-    if ex: 
+    if ex:
         return ex
-    if not(query):  return msg_error
-    return jsonify(_format(query) )
+    if not(query):
+        return msg_error
+    return jsonify(_format(query))
+
 
 @Estudiante.route('/programa/<int:programa>')
 @jwt_required()
@@ -45,73 +51,90 @@ def get_programa(programa):
     sql = "SELECT * FROM {} WHERE idprograma={};".format(tabla, programa)
     query = db.select(sql)
     ex = exception(query)
-    if ex: 
+    if ex:
         return ex
-    if not(query):  return msg_error
-    return jsonify(_format(query) )
+    if not(query):
+        return msg_error
+    return jsonify(_format(query))
 
 
 @Estudiante.route('/programa/<int:programa>/<int:periodo>')
 @jwt_required()
 def get_periodo_programa(programa, periodo):
-    sql = "SELECT * FROM {} WHERE REGISTRO={} and idprograma={}".format(tabla, periodo, programa)
+    sql = f'SELECT * FROM {tabla} WHERE REGISTRO={periodo} and idprograma={programa}'
     query = db.select(sql)
     ex = exception(query)
-    if ex: 
+    if ex:
         return ex
-    if not(query):  return msg_error
+    if not(query):
+        return msg_error
     return jsonify(_format(query))
+
 
 @Estudiante.route('/documento/<documento>')
 @jwt_required()
 def get_documento(documento):
-    sql = "SELECT * FROM {} WHERE documento='{}';".format(tabla,documento)
+    sql = "SELECT * FROM {} WHERE documento='{}';".format(tabla, documento)
     query = db.select(sql)
     ex = exception(query)
-    if ex: return ex
-    if not(query):  return msg_error
+    if ex:
+        return ex
+    if not(query):
+        return msg_error
     return jsonify(_format(query))
 
 
 @Estudiante.route('/periodos')
 @jwt_required()
 def get_periodos():
-    # Obtener datos desde la bd SQL server       
+    # Obtener datos desde la bd SQL server
     sql = 'SELECT DISTINCT REGISTRO FROM {};'.format(tabla)
     query = db.select(sql)
     ex = exception(query)
-    if ex: return ex
-    if not query: return msg_error
-    periodos_list = [ int(p['REGISTRO']) for p in query]
-    periodos =  sorted(periodos_list)
-    if not(periodos):  return msg_error
+    if ex:
+        return ex
+    if not query:
+        return msg_error
+    periodos_list = [int(p['REGISTRO']) for p in query]
+    periodos = sorted(periodos_list)
+    if not(periodos):
+        return msg_error
     return jsonify(_format(periodos))
+
 
 @Estudiante.route('/periodos/programa/<int:programa>')
 @jwt_required()
 def get_periodos_programa(programa):
-    # Obtener datos desde la bd SQL server       
+    # Obtener datos desde la bd SQL server
     sql = f'SELECT DISTINCT REGISTRO FROM {tabla} WHERE idprograma={programa};'
     query = db.select(sql)
     ex = exception(query)
-    if ex: return ex
-    if not query: return msg_error
-    periodos_list = [ int(p.get('REGISTRO')) for p in query]
-    periodos =  sorted(periodos_list)
-    if not(periodos):  return msg_error
+    if ex:
+        return ex
+    if not query:
+        return msg_error
+    periodos_list = [int(p.get('REGISTRO')) for p in query]
+    periodos = sorted(periodos_list)
+    if not(periodos):
+        return msg_error
     return jsonify(_format(periodos))
+
 
 @Estudiante.route('/programas')
 @jwt_required()
 def get_programas():
-    # Obtener datos desde la bd SQL server       
+    # Obtener datos desde la bd SQL server
     sql = 'SELECT DISTINCT idprograma, programa FROM {};'.format(tabla)
     query = db.select(sql)
     ex = exception(query)
-    if ex: return ex
-    if not query: return msg_error
-    programas_df = pd.DataFrame(query).sort_values(by='programa', ascending=True)
+    if ex:
+        return ex
+    if not query:
+        return msg_error
+    programas_df = pd.DataFrame(query).sort_values(
+        by='programa', ascending=True)
     programas_df['idprograma'] = programas_df['idprograma'].astype(int)
-    programas =  json.loads(programas_df.to_json(orient='records'))
-    if not(programas):  return msg_error
+    programas = json.loads(programas_df.to_json(orient='records'))
+    if not(programas):
+        return msg_error
     return jsonify(_format(programas))
--- api/controllers/desercion/Institucion.py
+++ api/controllers/desercion/Institucion.py
@@ -10,20 +10,24 @@ import json
 IES = Blueprint('IES', __name__)
 
 db = DB.getInstance()
-tabla = 'VWDATADESERCIONINSTITUCION'
-msg_error = { 'msg': 'No hay concidencias' }, 404
+tabla = 'VWDATADESERCION'
+msg_error = {'msg': 'No hay concidencias'}, 404
 
 ##########################################################  VWDATADESERCIONINSTITUCION ##########################################################
+
+
 @IES.route('/<int:periodo>')
 @jwt_required()
 def get_periodo(periodo: int):
     sql = 'SELECT * FROM {} WHERE periodo={};'.format(tabla, periodo)
     query = db.select(sql)
     ex = exception(query)
-    if ex: 
+    if ex:
         return ex
-    if not(query):  return msg_error
-    return jsonify(_format(query)) 
+    if not(query):
+        return msg_error
+    return jsonify(_format(query))
+
 
 @IES.route('/totales/<int:periodo>')
 @jwt_required()
@@ -31,10 +35,12 @@ def get_totales_periodo(periodo: int):
     sql = 'SELECT sum(desertores) AS desertores, avg(desercion) AS desercion, sum(egresados) AS egresados, sum(mat_hombre) AS mat_hombre, sum(mat_mujer) AS mat_mujer, sum(mat_total) AS mat_total, sum(admi_hombre) AS admi_hombre, sum(admi_mujer) AS admi_mujer, sum(admi_total) AS admi_total, sum(insc_hombre) AS insc_hombre, sum(insc_mujer) AS insc_mujer, sum(insc_total) AS insc_total, sum(mat_nuevos_hombre) AS mat_nuevos_hombre, sum(mat_nuevos_mujer) AS mat_nuevos_mujer, sum(mat_nuevos_total) AS mat_nuevos_total FROM {} WHERE periodo={};'.format(tabla, periodo)
     query = db.select(sql)
     ex = exception(query)
-    if ex: 
+    if ex:
         return ex
-    if not(query):  return msg_error
-    return jsonify(_format(query)) 
+    if not(query):
+        return msg_error
+    return jsonify(_format(query))
+
 
 @IES.route('/programa/<int:programa>')
 @jwt_required()
@@ -42,62 +48,81 @@ def get_programa(programa: int):
     sql = "SELECT * FROM {} WHERE idprograma={};".format(tabla, programa)
     query = db.select(sql)
     ex = exception(query)
-    if ex: 
+    if ex:
         return ex
-    if not(query):  return msg_error
-    return jsonify(_format(query)) 
+    if not(query):
+        return msg_error
+    return jsonify(_format(query))
+
 
 @IES.route('/programa/<int:programa>/<int:periodo>')
 @jwt_required()
 def get_periodo_programa(programa: int, periodo: int):
-    sql = "SELECT * FROM {} WHERE periodo={} and idprograma={}".format(tabla, periodo, programa)
+    sql = "SELECT * FROM {} WHERE periodo={} and idprograma={}".format(
+        tabla, periodo, programa)
     query = db.select(sql)
     ex = exception(query)
-    if ex: 
+    if ex:
         return ex
-    if not(query):  return msg_error
+    if not(query):
+        return msg_error
     return jsonify(_format(query))
+
 
 @IES.route('/periodos')
 @jwt_required()
 def get_periodos():
-    # Obtener datos desde la bd SQL server       
+    # Obtener datos desde la bd SQL server
     sql = 'SELECT DISTINCT periodo FROM {};'.format(tabla)
     query = db.select(sql)
     ex = exception(query)
-    if ex: return ex
-    if not query: return msg_error
-    periodos_list = [ int(p['periodo']) for p in query]
-    periodos =  sorted(periodos_list)
-    if not(periodos):  return msg_error
+    if ex:
+        return ex
+    if not query:
+        return msg_error
+    periodos_list = [int(p['periodo']) for p in query]
+    periodos = sorted(periodos_list)
+    if not(periodos):
+        return msg_error
     return jsonify(_format(periodos))
+
 
 @IES.route('/programas')
 @jwt_required()
 def get_programas():
-    # Obtener datos desde la bd SQL server       
+    # Obtener datos desde la bd SQL server
     sql = 'SELECT DISTINCT idprograma, programa FROM {};'.format(tabla)
     query = db.select(sql)
     ex = exception(query)
-    if ex: return ex
-    if not query: return msg_error
-    programas_df = pd.DataFrame(query).sort_values(by='programa', ascending=True)
+    if ex:
+        return ex
+    if not query:
+        return msg_error
+    programas_df = pd.DataFrame(query).sort_values(
+        by='programa', ascending=True)
     programas_df['idprograma'] = programas_df['idprograma'].astype(int)
-    programas =  json.loads(programas_df.to_json(orient='records'))
-    if not(programas):  return msg_error
+    programas = json.loads(programas_df.to_json(orient='records'))
+    if not(programas):
+        return msg_error
     return jsonify(_format(programas))
-    
+
+
 @IES.route('/programas/<int:periodo>')
 @jwt_required()
 def get_programas_by_periodo(periodo: int):
-    # Obtener datos desde la bd SQL server       
-    sql = 'SELECT DISTINCT idprograma, programa FROM {} WHERE periodo={};'.format(tabla, periodo)
+    # Obtener datos desde la bd SQL server
+    sql = 'SELECT DISTINCT idprograma, programa FROM {} WHERE periodo={};'.format(
+        tabla, periodo)
     query = db.select(sql)
     ex = exception(query)
-    if ex: return ex
-    if not query: return msg_error
-    programas_df = pd.DataFrame(query).sort_values(by='programa', ascending=True)
+    if ex:
+        return ex
+    if not query:
+        return msg_error
+    programas_df = pd.DataFrame(query).sort_values(
+        by='programa', ascending=True)
     programas_df['idprograma'] = programas_df['idprograma'].astype(int)
-    programas =  json.loads(programas_df.to_json(orient='records'))
-    if not(programas):  return msg_error
+    programas = json.loads(programas_df.to_json(orient='records'))
+    if not(programas):
+        return msg_error
     return jsonify(_format(programas))
