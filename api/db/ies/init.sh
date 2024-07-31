3332f0e8dc65313ddc6dbf9df1274446e1e6aede
Juan Quintero <juanestquintero@gmail.com>
2024-07-30 20:56:43 -0500

    chore(db):  sql server service script

--- api/db/ies/init.sh
+++ api/db/ies/init.sh
@@ -9,36 +9,31 @@ sqlcmd_user=$sqlcmd" $MSSQL_DBUSER -P $MSSQL_DBUSERPWD -i"
 
 
 ## Debugging: print variables
-echo "MSSQL_SA_PASSWORD: $MSSQL_SA_PASSWORD"
+echo -e "\nMSSQL_SA_PASSWORD: $MSSQL_SA_PASSWORD"
 echo "MSSQL_DBUSER: $MSSQL_DBUSER"
 echo "MSSQL_DBUSERPWD: $MSSQL_DBUSERPWD"
 
 
 ## Wait for SQL Server to be ready
-echo "Esperando a que SQL Server este listo..."
-sleep 90s
+echo -e "\nEsperando a que el servicio de SQL Server este arriba..."
+sleep 75s
 
 ## Check if database already exists
-echo "Checking if database $MSSQL_DBNAME exists..."
+echo -e "\nComprobando si la base de datos $MSSQL_DBNAME existe..."
 
 SQL_QUERY="IF DB_ID(N'$MSSQL_DBNAME') IS NOT NULL PRINT 'EXISTS'"
-echo "SQL Query: $SQL_QUERY"
-
 SQLCMD_OUTPUT=$(/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -Q "$SQL_QUERY" -W)
-echo "SQLCMD Output: $SQLCMD_OUTPUT"
-
 DB_EXISTS=$(echo $SQLCMD_OUTPUT | grep -o "EXISTS")
 
-echo "DB_EXISTS: $DB_EXISTS"
 
 if [[ $DB_EXISTS == "EXISTS" ]]; then
-    echo "The database '$MSSQL_DBNAME' already exists."
+    echo -e "\nLa base de datos '$MSSQL_DBNAME' ya existe."
 else
     ## Execute initial sql files
-    echo "Inicializando la base de datos de deserción..."
+    echo -e "\nCreando la bd/schema de deserción..."
     $sqlcmd_sa /tmp/sql/init.sql
 
-    echo "Ejecutando creates.sql..."
+    echo "Ejecutando creates tables..."
     $sqlcmd_sa /tmp/sql/creates.sql
 
     echo "Ejecutando inserts dependencias.sql..."
@@ -47,5 +42,5 @@ else
     echo "Ejecutando inserts desercion.sql..."
     $sqlcmd_user /tmp/sql/inserts/desercion.sql
 
-    echo "Finalizó la definicion de la bd de deserción!!!"
+    echo -e "\nFinalizó la definicion de la bd de deserción."
 fi
