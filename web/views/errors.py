1bb22a9636ff9c72bc51cf2a5065617b1c573f70
Juan Quintero <juanestquintero@gmail.com>
2024-08-13 11:23:50 -0500

    chore(web): format errores view file

--- web/views/errors.py
+++ web/views/errors.py
@@ -5,19 +5,23 @@ error_logger = logging.getLogger('error_logger')
 
 Error = Blueprint('Error', __name__)
 
+
 @Error.app_errorhandler(404)
 def page_not_found(e):
     return render_template('utils/error.html', error=str(e)), 404
 
+
 @Error.app_errorhandler(405)
 def method_not_allow(e):
     return render_template('utils/error.html', error=str(e)), 405
+
 
 @Error.app_errorhandler(500)
 @Error.route('/error')
 def handle_500(e):
     error_logger.error(e)
     return render_template('utils/error.html', error=str(e)), 500
+
 
 @Error.app_errorhandler(Exception)
 def handle_exception(e):
