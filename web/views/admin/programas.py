009fe722be3c50da7ca065b3c9197520db3bf19f
Juan Quintero <juanestquintero@gmail.com>
2024-08-26 19:47:19 -0400

    chore(web): format admin view academic program file

--- web/views/admin/programas.py
+++ web/views/admin/programas.py
@@ -7,22 +7,24 @@ Programa = Blueprint('Programa', __name__)
 
 endopoint = 'programas/'
 
+
 @Programa.route('/')
 @only_admin
 def listar():
-    status,body = get(endopoint)
+    status, body = get(endopoint)
     if status:
-        return render_template('admin/'+endopoint+'listar.html',programas=body)
+        return render_template('admin/'+endopoint+'listar.html', programas=body)
     else:
-        return render_template('admin/'+endopoint+'listar.html',programas=[], error=body)
+        return render_template('admin/'+endopoint+'listar.html', programas=[], error=body)
 
-@Programa.route('/detalle',methods=['POST'])
+
+@Programa.route('/detalle', methods=['POST'])
 @only_admin
 def detalle():
-    status_f,body_f = get('facultades')
+    status_f, body_f = get('facultades')
     body = dict(request.values)
     programa = literal_eval(body['programa'])
     if status_f and body:
-        return render_template('admin/'+endopoint+'detalle.html', p=programa,facultades=body_f,)
+        return render_template('admin/'+endopoint+'detalle.html', p=programa, facultades=body_f,)
     else:
         return render_template('utils/mensaje.html', mensaje='No se pudieron cargar las programas y las facultades', submensaje=body_f)
