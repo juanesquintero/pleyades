3e63957a40b442fb6b82076f02a8860248185204
Juan Quintero <juanestquintero@gmail.com>
2024-11-22 11:05:45 -0500

    fix(api): update init.sh for better SQL Server readiness checks and enhance variable debugging output

--- api/db/ies/init.sh
+++ api/db/ies/init.sh
@@ -1,45 +1,44 @@
-## Set file permissions
-chmod +x /tmp/init.sh
 
-## Define variables
-sqlcmd='/opt/mssql-tools/bin/sqlcmd -S localhost -U'
-
-sqlcmd_sa=$sqlcmd" sa -P $MSSQL_SA_PASSWORD -i"
-sqlcmd_user=$sqlcmd" $MSSQL_DBUSER -P $MSSQL_DBUSERPWD -i"
-
-
-## Debugging: print variables
+## Debugging: Print variables
 echo -e "\nMSSQL_SA_PASSWORD: $MSSQL_SA_PASSWORD"
 echo "MSSQL_DBUSER: $MSSQL_DBUSER"
 echo "MSSQL_DBUSERPWD: $MSSQL_DBUSERPWD"
+echo "MSSQL_DBNAME: $MSSQL_DBNAME"
 
+echo "Current user: $(whoami)"
+ls -ld /var/opt/mssql
 
 ## Wait for SQL Server to be ready
-echo -e "\nWaiting for SQL Server to be up..."
-sleep 75s
+echo -e "\nWaiting for SQL Server to be ready..."
+for i in {1..20}; do
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -Q "SELECT 1" &>/dev/null && break
+    echo "SQL Server not ready yet. Retrying in 10 seconds..."
+    sleep 10
+done
 
-## Check if database already exists
+## Check if database exists
 echo -e "\nChecking if the database $MSSQL_DBNAME exists..."
-
-SQL_QUERY="IF DB_ID(N'$MSSQL_DBNAME') IS NOT NULL PRINT 'EXISTS'"
+SQL_QUERY="IF DB_ID(N''$MSSQL_DBNAME'') IS NOT NULL PRINT 'EXISTS'"
 SQLCMD_OUTPUT=$(/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -Q "$SQL_QUERY" -W)
 DB_EXISTS=$(echo $SQLCMD_OUTPUT | grep -o "EXISTS")
-
 
 if [[ $DB_EXISTS == "EXISTS" ]]; then
     echo -e "\nThe database '$MSSQL_DBNAME' already exists."
 else
-    ## Execute initial sql files
     echo -e "\nCreating the dropout database/schema..."
+    envsubst < tmp/sql/init.sql > tmp/sql/init.sql
     $sqlcmd_sa /tmp/sql/init.sql
 
-    echo "Executing table creation scripts..."
+    echo -e "\nExecuting table creation scripts..."
+    envsubst < tmp/sql/creates.sql > tmp/sql/creates.sql
     $sqlcmd_sa /tmp/sql/creates.sql
 
-    echo "Executing dependency inserts..."
+    echo -e "\nExecuting dependency inserts..."
+    envsubst < tmp/sql/inserts/dependencies.sql > tmp/sql/inserts/dependencies.sql
     $sqlcmd_user /tmp/sql/inserts/dependencies.sql
 
-    echo "Executing dropout inserts..."
+    echo -e "\nExecuting dropout inserts..."
+    envsubst < tmp/sql/inserts/desertion.sql > tmp/sql/inserts/desertion.sql
     $sqlcmd_user /tmp/sql/inserts/desertion.sql
 
     echo -e "\nFinished setting up the dropout database."
