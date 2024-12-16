095360743c7bbcf12294829f95125f782d6c530e
Juan Quintero <juanestquintero@gmail.com>
2024-12-16 10:37:12 -0500

    refactor(web): restructure route configuration and add root routes in views initialization file

--- web/app.py
+++ web/app.py
@@ -1,5 +1,4 @@
-import datetime
-from flask import Flask, session, render_template, request, g
+from flask import Flask, request, g
 from flask_babel import Babel, _
 from views import add_app_routes
 
@@ -8,34 +7,9 @@ app = Flask(__name__, template_folder='templates', static_url_path='/static')
 
 # app_config(app)
 app.config.from_object('config')
-base_path = app.config['BASE_PATH']
-excel_enabled = app.config['EXCEL_ENABLED']
-ies = app.config['IES']
-
-
-@app.route(base_path)
-@app.route(base_path+'inicio')
-def inicio():
-    return render_template('utils/home.html'), 200
-
-
-@app.route(base_path+'contactanos')
-def contactanos():
-    return render_template('utils/contactanos.html', ies=ies), 200
-
 
 # Register routes
-add_app_routes(app, base_path)
-
-
-@app.before_request
-def before_each_request():
-    # Excel data loading enabled
-    session['excel'] = excel_enabled
-    # Session permanent
-    session.modified = True
-    session.permanent = True
-    app.permanent_session_lifetime = datetime.timedelta(hours=3)
+add_app_routes(app)
 
 
 def get_locale():
--- web/views/__init__.py
+++ web/views/__init__.py
@@ -6,9 +6,40 @@ from views.admin import ResultAdmin, DatasetAdmin, Faculty, Program, Student, Us
 from views.datasets import Dataset
 from views.results import Result
 # from views.dashboards import Tablero
+from flask import render_template, session
+import datetime
 
 
-def add_app_routes(app, base_path):
+def add_route_config(app, base_path):
+
+    excel_enabled = app.config.get('EXCEL_ENABLED')
+    ies = app.config.get('IES')
+
+    @app.before_request
+    def before_each_request():
+        # Excel data loading enabled
+        session['excel'] = excel_enabled
+        # Session permanent
+        session.modified = True
+        session.permanent = True
+        app.permanent_session_lifetime = datetime.timedelta(hours=3)
+
+    @app.route(base_path)
+    @app.route(base_path+'inicio')
+    def index():
+        return render_template('utils/home.html'), 200
+
+    @app.route(base_path+'contactanos')
+    def contact_us():
+        return render_template('utils/contact_us.html', ies=ies), 200
+
+
+def add_app_routes(app):
+    base_path = app.config.get('BASE_PATH')
+
+    # Add route configuration
+    add_route_config(app, base_path)
+
     # Register routes
     app.register_blueprint(Error, url_prefix=base_path)
     app.register_blueprint(Auth, url_prefix=base_path)
