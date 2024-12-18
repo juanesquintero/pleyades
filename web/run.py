4904a969ed258ec2ab9d68f3494ca2450070020f
Juan Quintero <juanestquintero@gmail.com>
2024-12-18 08:48:15 -0500

    feat(web): add support for running Flask app or Celery worker based on command-line argument flag

--- web/run.py
+++ web/run.py
@@ -1,7 +1,39 @@
-from app import create_app
+import debugpy
+import argparse
+from app import create_app, create_celery_app
+
+
+def start_flask_app():
+    ### Start the Flask application.###
+    app = create_app()
+    print("Starting Flask server...")
+    app.run(host='0.0.0.0', port=80, debug=True)
+    return app
+
+
+def start_celery_worker():
+    ### Start the Celery worker.###
+    app = start_flask_app()
+    celery = create_celery_app(app)
+    print("Starting Celery worker...")
+    celery.worker_main(argv=['worker', '--loglevel=info'])
+
 
 if __name__ == '__main__':
-    #  Create application
-    app = create_app()
-    # Run server
-    app.run(host='0.0.0.0', port=80)
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(
+        description="Run the application in different modes."
+    )
+    parser.add_argument(
+        "celery",
+        action="store_true",
+        help="Run as a Celery worker"
+    )
+
+    args = parser.parse_args()
+
+    # Determine what to run based on the --celery flag
+    if args.celery:
+        start_celery_worker()
+    else:
+        start_flask_app()
