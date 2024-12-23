76d49f4a15c85cb58d1a43a1c95bdee4b82718df
Juan Quintero <juanestquintero@gmail.com>
2024-12-23 10:34:57 -0500

    refactor(web): pass Flask app instance to start_flask_app and start_celery_worker for better context management

--- web/run.py
+++ web/run.py
@@ -1,31 +1,36 @@
-import debugpy
+from flask import Flask
 import argparse
 from app import create_app, create_celery_app
 
 
-def start_flask_app():
+def start_flask_app(app: Flask):
     ### Start the Flask application.###
-    app = create_app()
     print("Starting Flask server...")
-    app.run(host='0.0.0.0', port=80, debug=True)
-    return app
+    app.run(host='0.0.0.0', port=80, debug=True, use_reloader=True)
 
 
-def start_celery_worker():
-    ### Start the Celery worker.###
-    app = start_flask_app()
+def start_celery_worker(app: Flask):
+   # Create Celery instance with the shared Flask app
     celery = create_celery_app(app)
     print("Starting Celery worker...")
+
+    # Run Celery worker with the shared app context
+    # with app.app_context():
+    ### Start the Celery worker.###
     celery.worker_main(argv=['worker', '--loglevel=info'])
 
 
 if __name__ == '__main__':
+    # Create a single Flask app instance
+    app = create_app()
+
     # Set up argument parsing
     parser = argparse.ArgumentParser(
-        description="Run the application in different modes."
+        description="Run the flask application and celery worker."
     )
     parser.add_argument(
-        "celery",
+        "--celery",
+        "-c",
         action="store_true",
         help="Run as a Celery worker"
     )
@@ -34,6 +39,7 @@ if __name__ == '__main__':
 
     # Determine what to run based on the --celery flag
     if args.celery:
-        start_celery_worker()
+        # Start the Celery worker
+        start_celery_worker(app)
     else:
-        start_flask_app()
+        start_flask_app(app)
