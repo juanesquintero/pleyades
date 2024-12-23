9ae6e1dd6ec7f4e241fed36ac142286aa4151323
Juan Quintero <juanestquintero@gmail.com>
2024-12-23 10:42:27 -0500

    refactor(web): centralize Redis connection string in config and update session and Celery settings for improved maintainability

--- web/config.py
+++ web/config.py
@@ -14,6 +14,8 @@ sys.path.append('./')
 load_dotenv()
 
 
+REDIS_PATH = 'redis://pleyades-redis:6379'
+
 ##### LOGGING CONFIG #####
 LOG_FORMAT = '%(levelname)s %(asctime)s - %(message)s'
 # GENERAL (ALL) LOGS
@@ -40,7 +42,7 @@ model_logger.addHandler(file_handler)
 ##### SESSION CONFIG #####
 SECRET_KEY = os.getenv('SESSION_KEY')
 SESSION_TYPE = 'redis'
-SESSION_REDIS = Redis.from_url('redis://redis:6379')
+SESSION_REDIS = Redis.from_url(REDIS_PATH)
 SESSION_PERMANENT = True
 PERMANENT_SESSION_LIFETIME = datetime.timedelta(hours=3)
 SESSION_USE_SIGNER = True
@@ -54,9 +56,19 @@ BABEL_DEFAULT_TIMEZONE = 'UTC'
 LANGUAGES = ['en', 'es']
 #### END BABEL LANGS CONFIG ####
 
+
+#### ASYNC TASKS CONFIG ####
+CELERY = dict(
+    broker_url=REDIS_PATH,
+    result_backend=REDIS_PATH,
+    task_ignore_result=True,
+)
+#### END ASYNC TASKS CONFIG ####
+
 #### CUSTOM APP CONFIG ####
 IES = obtener_ies_config()
 IES_NAME = os.getenv('CLI_IES_NAME')
-BASE_PATH = '/'  # BASE_PATH = ('/' + ies_name) if ies_name else '/'
+BASE_PATH = '/' + IES_NAME if IES_NAME else '/'
+APPLICATION_ROOT = os.getenv('APPLICATION_ROOT', '/')
 EXCEL_ENABLED = os.getenv('EXCEL', 'false').lower() in ('true', '1', 't')
 #### END CUSTOM APP CONFIG ####
