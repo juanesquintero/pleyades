1900747f32eaf939d6d35d077bf34262923f7c4c
Juan Quintero <juanestquintero@gmail.com>
2025-02-10 14:12:42 -0500

    chore(tests): add fixtures for app, client, and runner; update test to check JSON response from root path

--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -1,13 +1,6 @@
-from playwright.sync_api import sync_playwright
 
 
-BASE_URL = "http://127.0.0.1:5001/"
 
-
-def test_root_path():
-    with sync_playwright() as p:
-        request = p.request.new_context()
-        response = request.get(BASE_URL)
-        assert response.ok
-        assert isinstance(response.json(), dict)
-        assert response.json().get('api') == 'pleyades'
+def test_json_data(client):
+    response = client.get("/", )
+    assert response.json.get("api") == "pleyades"
