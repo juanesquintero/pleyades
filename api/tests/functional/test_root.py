8f34474d54adffd09f42a946b3fc3879bb801047
Juan Quintero <juanestquintero@gmail.com>
2025-02-11 13:33:31 -0500

    chore(tests): remove unused imports from __init__.py and fix expected API response case in test_root.py

--- api/tests/functional/test_root.py
+++ api/tests/functional/test_root.py
@@ -1,4 +1,4 @@
 def test_json_data(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json.get("api") == "pleyades"
+    assert response.json.get("api") == "Pleyades"
