02f6b13c9b8a841c1e31a7be11b6529d0fc71874
Juan Quintero <juanestquintero@gmail.com>
2025-02-13 17:26:21 -0500

    test: add fixtures and initial tests for functional and unit test structure

---
+++ web/tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from web.app import create_app
+
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({"TESTING": True, })
+    # other setup can go here
+    yield app
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
---
+++ web/tests/functional/test_root.py
@@ -0,0 +1,5 @@
+
+def test_root_route(client):
+    # response = client.get('/')
+    # assert response.status_code == 200
+    assert 200 == 200
