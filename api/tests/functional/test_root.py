134670e895e318d1ad85c7bc411f1c01c819197d
Juan Quintero <juanestquintero@gmail.com>
2025-02-13 17:25:16 -0500

    test(api): rename test function for clarity

--- api/tests/functional/test_root.py
+++ api/tests/functional/test_root.py
@@ -1,4 +1,4 @@
-def test_json_data(client):
+def test_root_json_data(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json.get("api") == "Pleyades"
