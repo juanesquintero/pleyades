53501e93438ddf36d41ae721c354a62e66c60344
Juan Quintero <juanestquintero@gmail.com>
2025-02-18 11:56:49 -0500

    chore(update.sh): enhance update script with clear echo statements and improved container management

--- update.sh
+++ update.sh
@@ -1,7 +1,14 @@
-
+echo "Updating the codebase"
 git pull origin develop
 
+echo "Stopping the containers"
 docker-compose -f docker-compose.prod.yml stop
-&& docker-compose -f docker-compose.prod.yml rm -f
-&& docker-compose -f docker-compose.prod.yml down --rmi all
-&& docker-compose -f docker-compose.prod.yml up -d
+
+echo "Removing the containers"
+docker-compose -f docker-compose.prod.yml rm -f
+
+echo "Removing the images"
+docker-compose -f docker-compose.prod.yml down --rmi all
+
+echo "Building the containers"
+docker-compose -f docker-compose.prod.yml up -d
