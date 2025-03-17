bb64fdff8ef9f3db26b763710b1ec37edddd7ea0
Juan Quintero <juanestquintero@gmail.com>
2025-03-17 11:09:30 -0500

    feat(web): add task scheduling and error handling utilities

--- web/app/utils/tasks.py
+++ web/app/utils/tasks.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime
+import traceback
 from celery import shared_task
 
 
@@ -9,3 +10,17 @@ def sleep_task(seconds: int) -> datetime:
         print(f'Sleeping for {sec} seconds...')
         time.sleep(1)
     return datetime.now()
+
+
+@shared_task(ignore_result=False)
+def schedule_preparation_task(preparation_id: str) -> None:
+    pass
+
+
+def run_task(function_task, **kwargs) -> None:
+    try:
+        result = function_task.delay(**kwargs)
+        return result
+    except Exception as e:
+        error = str(e) + '\n' + traceback.format_exc()
+        return error
