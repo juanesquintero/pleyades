fa1518dd6ca43a2d6e08c98bf1b88413cd5dc23c
Juan Quintero <juanestquintero@gmail.com>
2025-03-18 00:03:05 -0500

    refactor(web): update task scheduling endpoints and improve error handling

--- web/app/views/tasks.py
+++ web/app/views/tasks.py
@@ -1,5 +1,4 @@
-import traceback
-from web.app.utils.tasks import sleep_task
+from web.app.utils.tasks import run_task, sleep_task, schedule_preparation_task
 from celery.result import AsyncResult
 from flask import Blueprint
 
@@ -9,17 +8,13 @@ Tasks = Blueprint('Tasks', __name__)
 
 @Tasks.route('/test/sleep/')
 @Tasks.route('/test/sleep/<int:seconds>')
-def set_schedule_task(seconds: int = 15):
-    try:
-        result = sleep_task.delay(seconds)
-    except Exception as e:
-        error = str(e) + '\n' + traceback.format_exc()
-        return {"error": error}
-    return {"result_id": result.id}
+def set_schedule_test_task(seconds: int = 15):
+    result = run_task(sleep_task, seconds=seconds)
+    return {"result": result}
 
 
 @Tasks.route('/test/result/<id>')
-def get_task_result(id: str) -> dict[str, object]:
+def get_test_task_result(id: str) -> dict[str, object]:
     result = AsyncResult(id)
     return {
         "ready": result.ready(),
